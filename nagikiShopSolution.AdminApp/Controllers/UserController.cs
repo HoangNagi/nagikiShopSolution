5291957a9540b5d130f9b719092abe3dcb465a42
Hoang Nhu <36804847+HoangNagi@users.noreply.github.com>
2020-05-19 16:09:23 +0700

    new commit

---
+++ nagikiShopSolution.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NagikiShopSolution.ViewModels.System.Users;
+
+namespace nagikiShopSolution.AdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
